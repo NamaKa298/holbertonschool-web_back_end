7856ca6409ad37ae54ec94eb5550a6fb5b445d66
NamaKa298 <marion.saint-martin@protonmail.com>
2024-07-18 23:05:47 +0200

    5. A Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,11 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
-    }
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
+    if (this.constructor !== Building && this.evacuationWarningMessage
+      === Building.prototype.evacuationWarningMessage)
+
+      if (typeof sqft !== 'number') {
+        throw new TypeError('sqft must be a number');
+      }
     this._sqft = sqft;
   }
 
