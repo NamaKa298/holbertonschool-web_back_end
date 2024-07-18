6d4291d790515a69e50b58cae9b5df0342c11a67
NamaKa298 <marion.saint-martin@protonmail.com>
2024-07-18 11:54:01 +0200

    10. For...of Loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (const [index, value] of array.entries()) {
+  for (const|let [index, value] of array.entries()) {
     newArray[index] = appendString + value;
   }
   return newArray;
