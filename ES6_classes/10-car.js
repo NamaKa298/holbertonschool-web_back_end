df1828ecc25b7fd57f6c92add34b341960e93add
NamaKa298 <marion.saint-martin@protonmail.com>
2024-07-19 11:54:05 +0200

    10. Vroom

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,53 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      throw new TypeError('brand must be a string');
+    }
+    this._brand = brand;
+    if (typeof motor !== 'string') {
+      throw new TypeError('motor must be a string');
+    }
+    this._motor = motor;
+    if (typeof color !== 'string') {
+      throw new TypeError('color must be a string');
+    }
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(marque) {
+    if (typeof marque !== 'string') {
+      throw new TypeError('brand must be a string');
+    }
+    this._brand = marque;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(moteur) {
+    if (typeof moteur !== 'string') {
+      throw new TypeError('motor must be a string');
+    }
+    this._motor = moteur;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(couleur) {
+    if (typeof couleur !== 'string') {
+      throw new TypeError('color must be a string');
+    }
+    this._color = couleur;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
