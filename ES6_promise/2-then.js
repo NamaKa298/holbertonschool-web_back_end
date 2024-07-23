adc0c6356a8ed6e35c3a50da1738f7284cf002eb
NamaKa298 <marion.saint-martin@protonmail.com>
2024-07-23 10:38:52 +0200

    2. Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-    if (promise === true) {
-        console.log('Got a response from the API');
-        return Promise.resolve({ status: 200, body: 'Success' });
-      }
-    return Promise.reject(new Error(''));   
+  if (promise === true) {
+    console.log('Got a response from the API');
+    return Promise.resolve({ status: 200, body: 'Success' });
+  }
+  return Promise.reject(new Error(''));
 }
