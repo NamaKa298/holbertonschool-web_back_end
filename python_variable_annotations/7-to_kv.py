a738124815b8df277167d48df952e7947ecd2184
NamaKa298 <marion.saint-martin@protonmail.com>
2024-08-12 16:31:37 +0200

    modif

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from typing import Union, Tuple
 """7. Complex types - string and int/float to tuple"""
+
+
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from typing import Iterable, Sequence, List, Tuple
 """9. Let's duck type an iterable object"""
+
+
+from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
