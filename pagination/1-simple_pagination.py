d4e9c9dfa261a3dc03b1b68eb2281d9e93367530
NamaKa298 <marion.saint-martin@protonmail.com>
2024-08-19 15:45:09 +0200

    1. Simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
 """1. Simple pagination"""
 import csv
-import math
 from typing import List
-
-
-def index_range(page: int, page_size: int) -> tuple:
-    """return a tuple of size two containing a start
-    index and an end index corresponding to the range of
-    indexes to return in a list for those particular
-    pagination parameters."""
-    return (page - 1) * page_size, page * page_size
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
