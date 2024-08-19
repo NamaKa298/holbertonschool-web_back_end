cfb9c7a6c67c15853f7e06470740c60735652de9
NamaKa298 <marion.saint-martin@protonmail.com>
2024-08-19 15:12:34 +0200

    0. Simple helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 """0. Simple helper function"""
 
 
-def index_range(page, page_size) -> tuple:
+def index_range(page: int, page_size: int) -> tuple:
     """return a tuple of size two containing a start
     index and an end index corresponding to the range of
     indexes to return in a list for those particular
