25bd79fa83ff6f8eeab907002f1de37de4b20a78
NamaKa298 <marion.saint-martin@protonmail.com>
2024-08-19 15:03:33 +0200

    0. Simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""0. Simple helper function"""
+
+
+def index_range(page, page_size) -> tuple:
+    """return a tuple of size two containing a start
+    index and an end index corresponding to the range of
+    indexes to return in a list for those particular
+    pagination parameters."""
+    return ((page - 1) * page_size, page * page_size)
