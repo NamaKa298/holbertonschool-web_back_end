8a36c7e8172d10b8bdc0bfda87e57746f2ff9d4b
NamaKa298 <marion.saint-martin@protonmail.com>
2024-08-19 10:15:10 +0200

    0. Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,6 +3,7 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
     for _ in range(10):
         await asyncio.sleep(1)
