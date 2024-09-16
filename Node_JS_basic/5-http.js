9bed6370cb055f58bc78698f8937282931dc4fbd
NamaKa298 <marion.saint-martin@protonmail.com>
2024-09-16 14:53:18 +0200

    5. Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const { Writable } = require('stream');
 const countStudents = require('./3-read_file_async');
 
 const database = process.argv[2];
@@ -10,13 +11,29 @@ const app = http.createServer((req, res) => {
   } else if (req.url === '/students' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
+
+    // Capture la sortie console.log()
+    const logCapture = [];
+    const logWritable = new Writable({
+      write(chunk, encoding, callback) {
+        logCapture.push(chunk.toString());
+        callback();
+      },
+    });
+
+    const originalStdoutWrite = process.stdout.write;
+
+    process.stdout.write = logWritable.write.bind(logWritable);
+
     countStudents(database)
-      .then((output) => {
-        res.write(output);
+      .then(() => {
+        process.stdout.write = originalStdoutWrite;
+        res.write(logCapture.join(''));
         res.end();
       })
       .catch((error) => {
-        res.write(error.message);
+        process.stdout.write = originalStdoutWrite;
+        res.write(`${error.message}\n`);
         res.end();
       });
   } else {
@@ -25,6 +42,8 @@ const app = http.createServer((req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 1245);
+app.listen(process.env.PORT || 1245, () => {
+  console.log('Serveur en cours d\'exécution sur le port 1245');
+});
 
 module.exports = app;
