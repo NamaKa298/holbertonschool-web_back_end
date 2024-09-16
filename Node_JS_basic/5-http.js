ca8e394d7185c7bf343085e74b6bd23692f9f673
NamaKa298 <marion.saint-martin@protonmail.com>
2024-09-16 14:37:41 +0200

    5. Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,40 +1,5 @@
 const http = require('http');
-const fs = require('fs').promises;
-
-async function countStudents(chemin) {
-  try {
-    const data = await fs.readFile(chemin, 'utf8');
-    const lignes = data.trim().split('\n');
-    if (lignes.length <= 1) {
-      throw new Error('Cannot load the database');
-    }
-
-    const students = {};
-    let totalStudents = 0;
-
-    for (let i = 1; i < lignes.length; i += 1) {
-      const line = lignes[i].trim();
-      if (line) {
-        const [firstname, , , field] = line.split(',');
-        if (!students[field]) {
-          students[field] = [];
-        }
-        students[field].push(firstname);
-        totalStudents += 1;
-      }
-    }
-
-    let output = `Number of students: ${totalStudents}\n`;
-    for (const field in students) {
-      if (Object.prototype.hasOwnProperty.call(students, field)) {
-        output += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
-      }
-    }
-    return output;
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-}
+const countStudents = require('./3-read_file_async');
 
 const database = process.argv[2];
 
