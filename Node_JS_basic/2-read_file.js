72949b999b2f85b4c473e44e42b6c7fa00bf3c73
NamaKa298 <marion.saint-martin@protonmail.com>
2024-09-16 11:00:13 +0200

    2. Reading a file synchronously with Node JS

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -16,21 +16,21 @@ function countStudents(chemin) {
   const students = {};
   let totalStudents = 0;
 
-  for (let i = 1; i < lignes.length; i++) {
+  for (let i = 1; i < lignes.length; i += 1) {
     const line = lignes[i].trim();
     if (line) {
-      const [firstname, lastname, age, field] = line.split(',');
+      const [firstname, , , field] = line.split(',');
       if (!students[field]) {
         students[field] = [];
       }
       students[field].push(firstname);
-      totalStudents++;
+      totalStudents += 1;
     }
   }
 
   console.log(`Number of students: ${totalStudents}`);
   for (const field in students) {
-    if (students.hasOwnProperty(field)) {
+    if (Object.prototype.hasOwnProperty.call(students, field)) {
       console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
     }
   }
