90b8b556e3cf6bbe175c31e73e3fcdeb2065f3fd
NamaKa298 <marion.saint-martin@protonmail.com>
2024-09-16 15:33:25 +0200

    6. Create a small HTTP server using Express

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+// Route for '/'
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(process.env.PORT || 1245);
+// Start server on port 1245
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
 
 module.exports = app;
