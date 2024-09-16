98bf587b12d6db3cd415445a4f8fc3cb2335b5c2
NamaKa298 <marion.saint-martin@protonmail.com>
2024-09-16 10:48:08 +0200

    2. Reading a file synchronously with Node JS

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,40 +1,39 @@
 const fs = require('fs');
-const readline = require('readline');
 
-function countStudents(database_name) {
-  let compteur = 0;
-  const students = {};
-  const rl = readline.createInterface({
-    input: fs.createReadStream(database_name),
-    output: process.stdout,
-    terminal: false
-  });
-
-  rl.on('line', (line) => {
-    if (compteur === 0 && line.includes(',')) {
-      compteur++; // Compter la ligne d'en-tête
-      return;
-    }
-    const [firstname, lastname, age, field] = line.split(',');
-    if (!students[field]) {
-      students[field] = [];
-    }
-    students[field].push(firstname);
-    compteur++;
-  });
-
-  rl.on('close', () => {
-    console.log(`Number of students: ${compteur - 1}`); // Soustraire la ligne d'en-tête
-    for (const field in students) {
-      if (students.hasOwnProperty(field)) {
-        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
-      }
-    }
-  });
-
-  rl.on('error', (err) => {
+function countStudents(chemin) {
+  let data;
+  try {
+    data = fs.readFileSync(chemin, 'utf8');
+  } catch (err) {
     throw new Error('Cannot load the database');
-  });
+  }
+
+  const lignes = data.trim().split('\n');
+  if (lignes.length <= 1) {
+    throw new Error('Cannot load the database');
+  }
+
+  const students = {};
+  let totalStudents = 0;
+
+  for (let i = 1; i < lignes.length; i++) {
+    const line = lignes[i].trim();
+    if (line) {
+      const [firstname, lastname, age, field] = line.split(',');
+      if (!students[field]) {
+        students[field] = [];
+      }
+      students[field].push(firstname);
+      totalStudents++;
+    }
+  }
+
+  console.log(`Number of students: ${totalStudents}`);
+  for (const field in students) {
+    if (students.hasOwnProperty(field)) {
+      console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+    }
+  }
 }
 
 module.exports = countStudents;
