45def2eb569424fd605e667eb52c1f1890b011f9
NamaKa298 <marion.saint-martin@protonmail.com>
2024-09-18 09:31:13 +0200

    5. Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,47 +1,30 @@
 const http = require('http');
-const { Writable } = require('stream');
 const countStudents = require('./3-read_file_async');
-const path = process.argv[2];
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 1245;
+const DATABASE = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
-
-    // Capture the console output
-    const captureStream = new Writable();
-    let output = '';
-    captureStream._write = (chunk, encoding, done) => {
-      output += chunk.toString();
-      done();
-    };
-
-    const originalConsoleLog = console.log;
-    console.log = (...args) => {
-      captureStream.write(`${args.join(' ')}\n`);
-    };
-
     try {
-      await countStudents(path);
-      res.write(output);
-    } catch (err) {
-      res.write('Cannot load the database');
-    } finally {
-      console.log = originalConsoleLog; // Restore original console.log
+      const { students, csStudents, sweStudents } = await countStudents(DATABASE);
+      res.write(`Number of students: ${students.length}\n`);
+      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+      res.end();
+    } catch (error) {
+      res.end(error.message);
     }
-
-    res.end();
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
+    res.writeHead(404);
+    res.end('Invalid request');
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
-});
-
+app.listen(PORT, HOSTNAME);
 module.exports = app;
