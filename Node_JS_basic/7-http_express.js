d620bf92fe33b7c170529843006460243f383c36
NamaKa298 <marion.saint-martin@protonmail.com>
2024-09-18 09:37:02 +0200

    7. Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const database = process.argv[2];
-const port = 1245;
 const app = express();
-module.exports = app;
+const port = 1245;
+const database = process.argv[2];
 
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
-
 app.get('/students', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  countStudents(database).then((data) => {
-    res.end(data.join('\n'));
-  }).catch((error) => {
-    res.end(`${error.message}`);
-  });
+  countStudents(database)
+    .then((data) => {
+      const { students, csStudents, sweStudents } = data;
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${students.length}\n`);
+      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+      res.statusCode = 200;
+      res.end();
+    })
+    .catch((error) => {
+      res.statusCode = 500;
+      res.send(`This is the list of our students\n${error.message}`);
+    });
 });
 
-app.listen(port);
+app.listen(port, () => {
+
+});
+module.exports = app;
